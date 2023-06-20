1d31f91323d9619a8a7c15d1b08110f24f4e0189
Jobin <120872413+jobin-b@users.noreply.github.com>
2023-06-20 02:56:30 -0400

    add login page

---
+++ src/components/app-components/ManagerLogin.js
@@ -0,0 +1,21 @@
+
+function ManagerLogin() {
+
+    return (
+        <form>
+            <div className="label-input-combo">
+                <label htmlFor="storeNum">Store Number</label>
+                <input id="storeNum" name="storeNum" type="number" placeholder="000000" required/>
+            </div>
+
+            <div className="label-input-combo">
+                <label htmlFor="password">Password</label>
+                <input id="password" name="password" type="password" placeholder="******" required/>
+            </div>
+
+            <button type="submit" id="submit">Submit</button>
+        </form>
+    );
+  }
+  
+  export default ManagerLogin;
---
+++ src/pages/App.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react"
+import ManagerLogin from "../components/app-components/ManagerLogin";
+import EmployeeLogin from "../components/app-components/EmployeeLogin";
+import "../styles/App.css"
+
+
+
+function App() {
+  const [state, setState] = useState("Manager");
+
+  const loginDisplay = () => {
+    if(state === "Manager") {
+      return <ManagerLogin/>;
+    } else if(state === "Employee"){
+      return <EmployeeLogin/>;
+    } else {
+      console.log("I've been graped");
+    }
+  }
+  
+  return (
+    <div className="login">
+      <h1>LOG IN</h1>
+      <div >
+        <button onClick={() => {setState("Manager")}}>Manager</button>
+        <button onClick={() => {setState("Employee")}}>Employee</button>
+      </div>
+      {loginDisplay()}
+    </div>
+  );
+}
+
+export default App;
+
+
+
