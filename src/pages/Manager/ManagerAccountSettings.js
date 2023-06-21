9c3f8f89d368f0da0664e5ca2e059b067e815c7e
Ashton Mamou <ashton22440@gmail.com>
2023-06-21 16:22:24 -0400

    add management settings framework

--- src/pages/Manager/ManagerAccountSettings.js
+++ src/pages/Manager/ManagerAccountSettings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ManagerNavbar from '../../components/manager-components/ManagerNavbar';
 
 import '../../styles/manager.css';
@@ -7,11 +7,49 @@ import '../../styles/manager.css';
 
 function ManagerAccountSettings() {
 
+    const [storeNum, setstoreNum] = useState("111111");
+    const [password, setPassword] = useState("password");
+    const[unsavedChanges, setUnsavedChanges] = useState(false);
+
+
+    const handlestoreNumChange = (event) => {
+        setstoreNum(event.target.value);
+        setUnsavedChanges(true);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        setUnsavedChanges(true);
+    };
+
+    const handleSave = (event) => {
+        setUnsavedChanges(false);
+        //backend stuff
+    };
+
     return (
 
         <div className="manager-body">
             <ManagerNavbar/>
-            <p>Manager Account Settings Page</p>
+            <h2>Change Account Settings</h2>
+
+            <div>
+                <label>storeNum:</label>
+                <input type="number" value={storeNum} onChange={handlestoreNumChange}/>
+            </div>
+
+            <div>
+                <label>password:</label>
+                <input type="password" value={password} onChange={handlePasswordChange}/>
+            </div>
+
+            {unsavedChanges && (
+                <div>
+                    <p>There are unsaved changes!!</p>
+                    <button onClick={handleSave}>Save Changes?</button>
+                </div>
+            )}
+
         </div>
 
     );
