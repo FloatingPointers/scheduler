b127d6ba50b96cbc62286c86db5cb7939914675a
Jobin <120872413+jobin-b@users.noreply.github.com>
2023-06-27 14:54:03 -0400

    changes

--- src/components/manager-components/scheduler-components/HourlyView.js
+++ src/components/manager-components/scheduler-components/HourlyView.js
@@ -1,5 +1,5 @@
-/*import React from "react";
-//import { differenceInHours } from 'date-fns'
+import React from "react";
+import { differenceInHours } from 'date-fns'
 
 function HourlyView(props) {
   let {shiftInfo} = props;
@@ -8,12 +8,16 @@ function HourlyView(props) {
   let endTime = new Date([shiftInfo.date + "T" + shiftInfo.startTime]);
   let totalHours = differenceInHours(startTime, endTime, Math.ceil); //Allows for hourly view setup
 
+  const hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+  
   return (
     <div className="hourly">
+      
       
     </div>
   );
 
 }
 
-export default HourlyView;*/
+export default HourlyView;
--- src/pages/Manager/ScheduleManager.js
+++ src/pages/Manager/ScheduleManager.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ManagerNavbar from '../../components/manager-components/ManagerNavbar';
 import '../../styles/manager.css';
+import HourlyView from "../../components/manager-components/scheduler-components/HourlyView";
 
 
 
@@ -26,12 +27,27 @@ function ScheduleManager() {
   
   let shiftInfo = info.shifts[0];
 
+  //Setting the start and end time for a shift using any approach
+  const [currentShift, setCurrentShift] = useState({
+    employee: "",
+    start: "",
+    end: ""
+  }); 
+
+  const setShiftTimes = () => {
+    setCurrentShift({
+      ...currentShift,
+      start: new Date()
+    });
+  }
+
   //NEEDS  MULTIPLE COMPONENT BREAKDOWN
   return (
 
     <div className="manager-body">
       <ManagerNavbar />
       <p>Schedule Manager Page</p>
+      <HourlyView shiftInfo={shiftInfo} />
     </div>
 
   );
