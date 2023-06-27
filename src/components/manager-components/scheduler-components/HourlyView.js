869d42c62f2756831183611e1bf54371e6aaeeb5
Jobin <120872413+jobin-b@users.noreply.github.com>
2023-06-27 18:12:06 -0400

    Create hourly view scheduler component Co-authored-by: banana2244 <ashton22440@gmail.com> Co-authored-by: RyanA3 <alsobrooks.ryan@gmail.com>

--- src/components/manager-components/scheduler-components/HourlyView.js
+++ src/components/manager-components/scheduler-components/HourlyView.js
@@ -1,23 +1,109 @@
-import React from "react";
-import { differenceInHours } from 'date-fns'
+import React, { useState } from "react";
+import { differenceInHours, format, add, compareAsc } from "date-fns";
+
+import "../../../styles/manager.css";
 
 function HourlyView(props) {
-  let {shiftInfo} = props;
-  
+  let { shiftInfo, currentShift } = props;
+  let selectionStartTime = currentShift.start ? new Date([shiftInfo.date + "T" + currentShift.start]) : null;
+  let selectionEndTime = currentShift.end ? new Date([shiftInfo.date + "T" + currentShift.end]) : null;
+  let setSelectionStartTime = (date) => {
+    props.setCurrentShift({
+      ...currentShift,
+      start: format(date, "HH:mm"),
+    });
+  }
+  let setSelectionEndTime = (date) => {
+    props.setCurrentShift({
+      ...currentShift,
+      end: format(date, "HH:mm"),
+    })
+  }
+
   let startTime = new Date([shiftInfo.date + "T" + shiftInfo.startTime]);
-  let endTime = new Date([shiftInfo.date + "T" + shiftInfo.startTime]);
-  let totalHours = differenceInHours(startTime, endTime, Math.ceil); //Allows for hourly view setup
+  let startTimeRound = new Date(startTime.getTime());
+  startTimeRound.setMinutes(0, 0, 0);
+  let endTime = new Date([shiftInfo.date + "T" + shiftInfo.endTime]);
+  let endTimeRound = new Date(endTime.getTime());
+  endTimeRound.setHours(
+    endTimeRound.getHours() + Math.round(endTimeRound.getMinutes() / 60)
+  );
+  endTimeRound.setMinutes(0, 0, 0); // Resets also seconds and milliseconds
+  let totalHours = differenceInHours(endTimeRound, startTimeRound, Math.ceil); //Allows for hourly view setup
 
-  const hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  console.log(endTime);
 
-  
+  //Handles when a user selects an hour element
+  function handleHourSelection(index) {
+    // Number of hours from startTime
+    if (selectionStartTime === null) {
+      let newStart = new Date(startTime.getTime());
+      newStart = add(newStart, { hours: index });
+      setSelectionStartTime(newStart);
+
+    } else if (selectionEndTime === null) {
+      let newEnd = new Date(startTime.getTime());
+      newEnd = add(newEnd, { hours: index });
+
+      //Check if the second selection is less than the first one, then swap start and end times
+      if (compareAsc(selectionStartTime, newEnd) === 1) {
+        let swap = selectionStartTime;
+        setSelectionStartTime(newEnd);
+        setSelectionEndTime(swap);
+      } else {
+        setSelectionEndTime(newEnd);
+      }
+
+    } else {
+      //Clear selection if both start and end times are selected
+      setSelectionStartTime(null);
+      setSelectionEndTime(null);
+    }
+  }
+
+  //Create a list containing an html element for each hour of the day, starting at the specified start time of the shift
+  function generateHoursContent() {
+    let hoursContent = [];
+    let cur_date = new Date(startTimeRound);
+
+    for (let hr = 0; hr < totalHours; hr++) {
+      let selected =
+        selectionStartTime !== null &&
+        selectionEndTime !== null &&
+        compareAsc(cur_date, selectionStartTime) === 1 &&
+        compareAsc(cur_date, selectionEndTime) === -1;
+
+      let selection_start = selectionStartTime !== null && cur_date.getHours() === selectionStartTime.getHours();
+      let selection_end = selectionEndTime !== null && cur_date.getHours() === selectionEndTime.getHours();
+
+      let classList = "";
+      if(selected) classList += "selected";
+      if(selection_start) classList += "selected-start";
+      if(selection_end) classList += "selected-end";
+
+      hoursContent.push(
+        <li
+          id={"hour-selector-" + cur_date.getHours()}
+          className={classList}
+          onClick={() => {
+            handleHourSelection(hr); //its like abstraction
+          }}
+        >
+          {format(cur_date, "h a")}
+        </li>
+      );
+
+      cur_date = add(cur_date, { hours: 1 });
+    }
+
+    return hoursContent;
+  }
+
   return (
     <div className="hourly">
-      
-      
+      <ul id="hour-display">{generateHoursContent()}</ul>
     </div>
   );
-
 }
 
 export default HourlyView;
--- src/pages/Manager/ScheduleManager.js
+++ src/pages/Manager/ScheduleManager.js
@@ -10,36 +10,69 @@ function ScheduleManager() {
 
   //Format type
   let [info, setInfo] = useState({
-      _id: "ObjectId",
-      storeId: "ObjectId", // References the store the schedule belongs to
-      employeeId: "ObjectId", // References the employee the schedule belongs to (optional)
-      weekStartDate: "Date",
-      shifts: [
-        {
-          date:"YYYY-MM-DD",
-          dayOfWeek: 0, // 0-6 representing Sunday-Saturday
-          startTime: "HH:MM",
-          endTime: "Date"
-        },
-        // Other shift-related fields
-      ]
+    _id: "ObjectId",
+    storeId: "ObjectId", // References the store the schedule belongs to
+    weekStartDate: "Date",
+    format: [
+      {
+        date: "2000-01-01",
+        dayOfWeek: 0, // 0-6 representing Sunday-Saturday
+        startTime: "00:00", //HH:MM
+        endTime: "24:00",
+        //settings
+      },
+      // Other shift-related fields
+    ],
+    schedule: [
+      {
+        goalsMet: false,
+        markedAsComplete: false,
+
+        shifts: [
+          {
+            employeeId: 1,
+            startTime: "00:00",
+            endTime: "05:00",
+          },
+          {
+            employeeId: 2,
+            startTime: "05:00",
+            endTime: "12:00",
+          },
+        ],
+        totalHours: 13,
+        totalCost: 4000,
+
+      },
+      {
+        goalsMet: false,
+        markedAsComplete: false,
+
+        shifts: [
+          {
+            employeeId: 1,
+            startTime: "05:00",
+            endTime: "14:00",
+          },
+        ],
+      },
+    ],
   });
   
-  let shiftInfo = info.shifts[0];
+  let shiftInfo = info.shiftTimes[0];
 
   //Setting the start and end time for a shift using any approach
+  //Shift currently being edited / created
   const [currentShift, setCurrentShift] = useState({
     employee: "",
     start: "",
     end: ""
   }); 
 
-  const setShiftTimes = () => {
-    setCurrentShift({
-      ...currentShift,
-      start: new Date()
-    });
-  }
+
+
+
+
 
   //NEEDS  MULTIPLE COMPONENT BREAKDOWN
   return (
@@ -47,7 +80,7 @@ function ScheduleManager() {
     <div className="manager-body">
       <ManagerNavbar />
       <p>Schedule Manager Page</p>
-      <HourlyView shiftInfo={shiftInfo} />
+      <HourlyView shiftInfo={shiftInfo} currentShift={currentShift} setCurrentShift={setCurrentShift}/>
     </div>
 
   );
