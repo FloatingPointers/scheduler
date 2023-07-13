555846622753b8d81ca91fd5f5275a9bc40a2a03
RyanA3 <alsobrooks.ryan@gmail.com>
2023-07-13 14:40:24 -0700

    Add minimal css to login page

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -51,33 +51,32 @@ function App() {
   }
 
   
-  
+
   return (
-    <div className="flex flex-row justify-center border-black border-0">
-      <h1>Log In</h1>
+    <div className="flex flex-row justify-center items-center h-screen w-screen
+                  bg-bgPrimary text-2xl">
       
-      <form onSubmit={login}>
-        <div>
-          <div className="label-input-combo">
-            <label htmlFor="username" className="text-blue-600">Username or email address</label>
-            <input name="username" type="text" required />
-          </div>
-        </div>
+      
+      <form onSubmit={login} className="border border-secondary bg-bgSecondary shadow-md drop-shadow p-8 flex flex-col gap-2">
+        <label htmlFor="username">Username or email address</label>
+        <input name="username" type="text" className="border border-secondary focus:border-accent focus:outline-none" required />
 
-        <div className="label-input-combo">
-          <label htmlFor="password">Password</label>
-          <input id="password" name="password" type="password" required/>
-        </div>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" className="border border-secondary focus:border-accent focus:outline-none" required/>
+        
+        <p>Login As: </p>
+        <ul className="bg-bgPrimary grid w-full gap-2 md:grid-cols-2"> 
+          <li>
+            <input type="radio" id="EMPLOYEE" name="loginType" value="EMPLOYEE" defaultChecked className="hidden peer"/>
+            <label htmlFor="EMPLOYEE" className="inline-flex items-center justify-center w-full p-1 cursor-pointer bg-bgSecondary border border-secondary rounded hover:bg-bgPrimary hover:border-primary peer-checked:bg-bgAccent peer-checked:border-accent peer-checked:text-accent">Employee</label>
+          </li>
+          <li>
+            <input type="radio" id="STORE" name="loginType" value="STORE" className="hidden peer"/>
+            <label htmlFor="STORE" className="inline-flex items-center justify-center w-full p-1 cursor-pointer bg-bgSecondary border border-secondary rounded hover:bg-bgPrimary hover:border-primary peer-checked:bg-bgAccent peer-checked:border-accent peer-checked:text-accent">Manager</label>
+          </li>
+        </ul>
 
-        <div>
-          <p>Login As: </p>
-          <input type="radio" id="EMPLOYEE" name="loginType" value="EMPLOYEE" defaultChecked/>
-          <label htmlFor="EMPLOYEE">Employee</label>
-          <input type="radio" id="STORE" name="loginType" value="STORE" />
-          <label htmlFor="STORE">Manager</label>
-        </div>
-
-        <button type="submit" id="submit">Submit</button>
+        <button type="submit" id="submit" className="text-3xl font-semibold p-2 m-2 bg-bgPrimary">Sign In</button>
       
         <NavLink to="/CreateStoreAccount/" className="create-account">Don't have an account?</NavLink>
       </form>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  mode: 'jit',
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgPrimary: '#f8fafc', //slate-50
+        bgSecondary: '#f1f5f9', //slate-100
+        bgAccent: '#d1fae5', //emerald-100
+        primary: '#475569', //slate-600
+        secondary: '#334155', //slate-700
+        accent: '#10b981' //emerald-500
+      },
+    }
   },
   plugins: [],
 }
