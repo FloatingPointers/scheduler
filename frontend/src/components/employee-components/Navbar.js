0b90b461d73345275852c757faeef918f5cd46ea
Ashton Ashton <ashton22440@gmail.com>
2023-08-02 01:47:15 -0400

    add highlight to selected navbar item

--- frontend/src/components/employee-components/Navbar.js
+++ frontend/src/components/employee-components/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/");
     localStorage.removeItem("role");
   };
+
+  const defaultClasses =
+    "px-4 py-1 text-center rounded hover:shadow-md hover:bg-slate-100 hover:shadow-slate-300 dark:hover:bg-slate-700 dark:hover:shadow-slate-800 transition-all";
+  const activeClasses = " bg-slate-100 dark:bg-slate-700 ";
 
   return (
     <div className="flex w-full justify-evenly items-center text-lg p-2 bg-slate-200 dark:bg-slate-600 dark:text-white">
@@ -24,25 +29,33 @@ function Navbar() {
       <nav className="inline-flex w-3/4 justify-end gap-6 mr-12 items-center">
         <NavLink
           to="/emp"
-          className="px-4 py-1 text-center rounded hover:shadow-md hover:bg-slate-100 hover:shadow-slate-300 dark:hover:bg-slate-700 dark:hover:shadow-slate-800 transition-all"
+          className={`${defaultClasses} ${
+            location.pathname === "/emp" ? activeClasses : ""
+          }`}
         >
           Home
         </NavLink>
         <NavLink
           to="/emp/schedule/"
-          className="px-4 py-1 text-center rounded hover:shadow-md hover:bg-slate-100 hover:shadow-slate-300 dark:hover:bg-slate-700 dark:hover:shadow-slate-800 transition-all"
+          className={`${defaultClasses} ${
+            location.pathname === "/emp/schedule/" ? activeClasses : ""
+          }`}
         >
           Schedule
         </NavLink>
         <NavLink
           to="/emp/request/"
-          className="px-4 py-1 text-center rounded hover:shadow-md hover:bg-slate-100 hover:shadow-slate-300 dark:hover:bg-slate-700 dark:hover:shadow-slate-800 transition-all"
+          className={`${defaultClasses} ${
+            location.pathname === "/emp/request/" ? activeClasses : ""
+          }`}
         >
           Requests
         </NavLink>
         <NavLink
           to="/emp/settings/"
-          className="px-4 py-1 text-center rounded hover:shadow-md hover:bg-slate-100 hover:shadow-slate-300 dark:hover:bg-slate-700 dark:hover:shadow-slate-800 transition-all"
+          className={`${defaultClasses} ${
+            location.pathname === "/emp/settings/" ? activeClasses : ""
+          }`}
         >
           Settings
         </NavLink>
